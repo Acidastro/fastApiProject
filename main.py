c32be51ee400e172292e81f8757d7e3e51cb5e7d
acaidastro <zassona@yandex.ru>
2022-10-19 17:58:55 +0300

    - Body() - Path()

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, Query
-from schemas import Book
+from fastapi import FastAPI, Query, Path, Body
+from schemas import Book, Author  # Модель валидации
 
 app = FastAPI()
 
@@ -19,11 +19,50 @@ def get_user_item(pk: int, item: str):
     return {'user': pk, 'item': item}
 
 
+def create_author(author: Author = Body(..., embed=True)):
+    """
+    Создает автора по модели pydantic
+    Body(..., embed=True) формирует ключ "author": {} внутри тела
+    """
+    return {
+        'author': author
+    }
+
+
 @app.post('/book')
-def create_book(item: Book):
-    return item
+def create_book(book: Book, author: Author, amount: int = Body(...)):
+    """
+    Данные item должны соответствовать модели, описанной в классе Book (pydantic)
+    Body позволяет добавить параметр в body запроса, а не в url адрес.
+    """
+    return {
+        'book': book,  # заполняем инфу по Book
+        'author': author,  # заполняем инфу по Author
+        'amount': amount,  # аргумент входит в тело запроса, и не учитывается в url
+    }
 
 
 @app.get('/book/')
-def get_book(q: str = Query(None, min_length=2, max_length=5, description='Search book')):
+def get_book(q: str = Query(None, min_length=2, max_length=5, description='Search book', deprecated=True)):
+    """
+    Query позволяет поставить ограничения ввода. Установлено ограничение от 2 до 5 символов.
+    description позволяет задать описание этой q.
+    Query(...): три точки указывают на то, что параметр обязательный.
+    Query('test') указывает значение по умолчанию.
+    List[str] = Query можно использовать для передачи списка параметров
+    Query(['asd'],['qwe']) установить список по умолчанию
+    depricated = True устанавливает в документации параметр "устаревший"
+    """
     return q
+
+
+@app.get('/book/{pk}')
+def get_single_book(pk: int = Path(..., gt=1, le=20), pages: int = Query(None, gt=10, le=500)):
+    """
+    gt=1 минмальное значение
+    le=20 максимальное значение
+    Разница:
+    Query = ?pages=20
+    Path = /book/5
+    """
+    return {'pk': pk, "pages": pages}
--- schemas.py
+++ schemas.py
@@ -1,3 +1,6 @@
+"""
+Файл содержит pydantic модели, для сериализации и валидации данных
+"""
 from typing import List
 
 from pydantic import BaseModel
@@ -8,7 +11,16 @@ class Genre(BaseModel):
     name: str
 
 
+class Author(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+
+
 class Book(BaseModel):
+    """
+    Основываясь на этой модели, pydantic будет валидировать данные, которые проходят через него.
+    """
     title: str
     writer: str
     duration: str
